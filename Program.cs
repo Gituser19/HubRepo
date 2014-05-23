c4c21487227986a8ca3b648598d393fa16195b96
Ramya <ramya.nandakumar@non.agilent.com>
2014-05-23 16:47:26 -0700

    modified pgm.cs before push

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main()
         {
             Console.WriteLine("This is my demo on Git!!!");
+            Console.WriteLine("This is my second change before push");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
